99930c11683b6a070393d8525565a0c49877034f
Cordavi <michaelamundsen@gmail.com>
2016-07-26 15:42:23 -0400

    Wrote basic NSURL request that gets back a response from github

--- github-repo-list-swift/ReposTableViewController.swift
+++ github-repo-list-swift/ReposTableViewController.swift
@@ -9,15 +9,35 @@
 import UIKit
 
 class ReposTableViewController: UITableViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.tableView.accessibilityLabel = "tableView"
-        
-    }
-
-    // MARK: - Table view data source
- 
-
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      self.tableView.accessibilityLabel = "tableView"
+      
+      let urlString = "https://api.github.com/repositories?client_id=" + "\(APIKeys.clientID)" + "&client_secret=" + "\(APIKeys.clientSecret)"
+      
+      guard let url = NSURL(string: urlString) else {
+         return
+      }
+      
+      let urlSession = NSURLSession.sharedSession()
+      urlSession.dataTaskWithURL(url) {data, response, error in
+         guard let data = data where error == nil else {
+            return
+         }
+         
+         do {
+            let responseData = try NSJSONSerialization.JSONObjectWithData(data, options: [.AllowFragments]) as? [[String : AnyObject]]
+            print(responseData)
+         } catch {
+            print(error)
+         }
+      }.resume()
+      
+   }
+   
+   // MARK: - Table view data source
+   
+   
 }
