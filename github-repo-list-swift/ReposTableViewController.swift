ccda8cb1f518811bd799d6ff2de43de6a9a948d2
Cordavi <michaelamundsen@gmail.com>
2016-07-26 16:09:22 -0400

    Tableview will display name of repository in tableview once data is downloaded

--- github-repo-list-swift/ReposTableViewController.swift
+++ github-repo-list-swift/ReposTableViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class ReposTableViewController: UITableViewController {
    
+   var gitHubData: [[String : AnyObject]] = []
+   
    override func viewDidLoad() {
       super.viewDidLoad()
       
       self.tableView.accessibilityLabel = "tableView"
+      self.tableView.delegate = self
       
       let urlString = "https://api.github.com/repositories?client_id=" + "\(APIKeys.clientID)" + "&client_secret=" + "\(APIKeys.clientSecret)"
       
@@ -28,16 +31,30 @@ class ReposTableViewController: UITableViewController {
          }
          
          do {
-            let responseData = try NSJSONSerialization.JSONObjectWithData(data, options: [.AllowFragments]) as? [[String : AnyObject]]
-            print(responseData)
+            let responseData = try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [[String: AnyObject]]
+            if let responseData = responseData {
+               self.gitHubData = responseData
+            }
+            self.tableView.reloadData()
+//            print(responseData?[0]["name"])
          } catch {
             print(error)
          }
       }.resume()
-      
    }
    
    // MARK: - Table view data source
+   
+   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return gitHubData.count
+   }
+   
+   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCellWithIdentifier("repoCell", forIndexPath: indexPath)
+      
+      cell.textLabel?.text = gitHubData[indexPath.row]["name"] as? String
+      return cell
+   }
    
    
 }
