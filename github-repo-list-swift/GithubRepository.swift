153d7cb8d4c6dee1773bec8e15c6685cc7dec9f5
Cordavi <michaelamundsen@gmail.com>
2016-07-27 22:36:20 -0400

    Refactored GitHubRepo initalizer

--- github-repo-list-swift/GithubRepository.swift
+++ github-repo-list-swift/GithubRepository.swift
@@ -9,44 +9,13 @@
 import UIKit
 
 class GithubRepository {
-   var fullName = String()
-   var htmlURL = NSURL()
-   var repositoryID = String()
+   let fullName: String
+   let htmlURL: NSURL
+   let repositoryID: String
    
    init(dictionary: [String: AnyObject]) {
-      self.fullName = checkFullName(dictionary)
-      self.htmlURL = checkhtmlURL(dictionary)
-      self.repositoryID = checkRepositoryID(dictionary)
-   }
-   
-   func checkFullName(dictionary: [String: AnyObject]) -> String {
-      let fullNameObject = dictionary["full_name"]
-      guard let fullName = fullNameObject where String(fullNameObject) != nil else {
-         return ""
-      }
-      let fullNameString = String(fullName)
-      return fullNameString
-   }
-   
-   func checkhtmlURL(dictionary: [String: AnyObject]) -> NSURL {
-      let htmlURLObject = dictionary["html_url"]
-      guard let htmlURL = htmlURLObject where String(htmlURLObject) != nil else {
-         return NSURL()
-      }
-
-      let htmlURLNSURL = NSURL(string: String(htmlURL))
-      guard let htmlRepoURL = htmlURLNSURL else {
-         return NSURL()
-      }
-      return htmlRepoURL
-   }
-   
-   func checkRepositoryID(dictionary: [String: AnyObject]) -> String {
-      let repoIDObject = dictionary["id"]
-      guard let repoID = repoIDObject where String(repoIDObject) != nil else {
-         return ""
-      }
-      let repoIDString = String(repoID)
-      return repoIDString
+      fullName = dictionary["full_name"] as? String ?? ""
+      htmlURL = NSURL(string: (dictionary["html_url"] as? String ?? "")) ?? NSURL()
+      repositoryID = (dictionary["id"] as? NSNumber)?.stringValue ?? ""
    }
 }
